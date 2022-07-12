acbbb8f5f4bf3b6a007101d84f92b76499f0130c
room-27 <room27projects@gmail.com>
2022-07-12 17:33:54 +0200

    Update model, CSS gradient \* Separate 'watch' script \* Attempt at a starting view \* Styling in style.css only

--- public/js/index.js
+++ public/js/index.js
@@ -26,8 +26,18 @@ const camera = new THREE.PerspectiveCamera(
   1,
   10000
 );
-camera.position.set(750.0, 630.0, 170.0);
+
+const cameraStart = new THREE.Vector3(-850.0, 700.0, -340.0);
+const cameraDirection = new THREE.Vector3(
+  -1.6196953785926882,
+  -0.6711233295446031,
+  -1.6493299762217073
+);
+
+camera.position.copy(cameraStart);
+camera.rotation.copy(cameraDirection);
 camera.updateMatrix();
+camera.updateProjectionMatrix();
 scene.add(camera);
 
 // Testing Cube
@@ -40,37 +50,35 @@ scene.add(camera);
 // Load Mesh
 var rock, rockWire, rockGroup;
 const wireMaterial = new THREE.MeshBasicMaterial({
-  color: 0xff00ff,
+  color: 0x333333,
   polygonOffset: true,
   polygonOffsetFactor: 1,
-  polygonOffsetUnits: 1
+  polygonOffsetUnits: 1,
   // wireframe: true,
   // wireframeLinewidth: 2
 });
 const meshLoader = new GLTFLoader();
 meshLoader.load(
-  "../models/wiregibsolid.gltf",
+  "../models/wiregib.gltf",
   (gltf) => {
     rock = gltf.scene;
     rock.traverse((node) => {
       if (!node.isMesh) return;
       node.material = wireMaterial;
     });
-    
-    rock.children[2].updateMatrix();
-    rock.children[2].scale.set(1, 1, 1);
-    rock.children[2].updateMatrix();
-    // scene.add(rock);
-    
-    let rockGeo = new THREE.WireframeGeometry(rock.children[2].geometry);
-    console.log(rock.children[2].geometry);
+    // Although scene has only one element usually, it may have a Light, Camera etc.,
+    // that I may not remember to remove (when updating mesh).
+
+    let rockGeo = new THREE.WireframeGeometry(rock.children[0].geometry); // Copy geometry, render wireframe on top of original.
+    console.log(rock.children[0].geometry);
     rockWire = new THREE.LineSegments(rockGeo);
     rockWire.material.color.set(0xffffff);
-    rockWire.material.linewidth = 2;
-    // scene.add(rockWire);
+    rockWire.material.linewidth = 1;
+
+    rockWire.updateMatrix();
 
     rockGroup = new THREE.Group();
-    rockGroup.add(rock.children[2]);
+    rockGroup.add(rock.children[0]);
     rockGroup.add(rockWire);
 
     scene.add(rockGroup);
@@ -78,7 +86,7 @@ meshLoader.load(
     console.log(rockGroup);
   },
   (xhr) => {
-    console.log(`${((xhr.loaded / xhr.total) * 100).toFixed(2)}% loaded`);
+    console.log(`${((xhr.loaded / xhr.total) * 100).toFixed(2)}% loaded`); // Progress indicator
   },
   (error) => {
     console.error(error);
@@ -90,22 +98,26 @@ let canvas = document.getElementsByClassName("webgl")[0];
 
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
-  antialias: true
+  antialias: true,
 });
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(dim.width, dim.height);
+renderer.setClearColor(0x000000, 0.0);
 
+// Controls
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.target.set(1000, 0, 0);
+
+const cameraTarget = cameraStart.add(cameraDirection.multiplyScalar(100)); // Add direction vector to start to obtain a point in this direction to target
+console.log(cameraStart, cameraDirection);
+console.log(cameraTarget);
 controls.addEventListener("change", () => renderer.render(scene, camera));
+
+camera.updateProjectionMatrix();
 
 // Main Loop
 const loop = () => {
   renderer.render(scene, camera);
   requestAnimationFrame(loop);
-
-  // console.log(camera.position, camera.rotation)
-
-  // rockGroup.rotation.y += 0.001;
+  console.log(camera)
 };
 loop();
