8d0b147f11f261f1143b6ea5832e898d39b49c3f
room-27 <room27projects@gmail.com>
2022-07-12 15:49:41 +0200

    Wireframe exterior only

--- public/js/index.js
+++ public/js/index.js
@@ -26,7 +26,8 @@ const camera = new THREE.PerspectiveCamera(
   1,
   10000
 );
-camera.position.z = 10;
+camera.position.set(750.0, 630.0, 170.0);
+camera.updateMatrix();
 scene.add(camera);
 
 // Testing Cube
@@ -37,11 +38,14 @@ scene.add(camera);
 // scene.add(cube);
 
 // Load Mesh
-var rock;
+var rock, rockWire, rockGroup;
 const wireMaterial = new THREE.MeshBasicMaterial({
-  color: 0xffffff,
-  wireframe: true,
-  wireframeLinewidth: 2
+  color: 0xff00ff,
+  polygonOffset: true,
+  polygonOffsetFactor: 1,
+  polygonOffsetUnits: 1
+  // wireframe: true,
+  // wireframeLinewidth: 2
 });
 const meshLoader = new GLTFLoader();
 meshLoader.load(
@@ -52,9 +56,26 @@ meshLoader.load(
       if (!node.isMesh) return;
       node.material = wireMaterial;
     });
-    rock.rotation.x = 0.2;
-    scene.add(rock);
-    console.log(rock);
+    
+    rock.children[2].updateMatrix();
+    rock.children[2].scale.set(1, 1, 1);
+    rock.children[2].updateMatrix();
+    // scene.add(rock);
+    
+    let rockGeo = new THREE.WireframeGeometry(rock.children[2].geometry);
+    console.log(rock.children[2].geometry);
+    rockWire = new THREE.LineSegments(rockGeo);
+    rockWire.material.color.set(0xffffff);
+    rockWire.material.linewidth = 2;
+    // scene.add(rockWire);
+
+    rockGroup = new THREE.Group();
+    rockGroup.add(rock.children[2]);
+    rockGroup.add(rockWire);
+
+    scene.add(rockGroup);
+
+    console.log(rockGroup);
   },
   (xhr) => {
     console.log(`${((xhr.loaded / xhr.total) * 100).toFixed(2)}% loaded`);
@@ -69,16 +90,22 @@ let canvas = document.getElementsByClassName("webgl")[0];
 
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
+  antialias: true
 });
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(dim.width, dim.height);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(1000, 0, 0);
+controls.addEventListener("change", () => renderer.render(scene, camera));
 
 // Main Loop
 const loop = () => {
   renderer.render(scene, camera);
   requestAnimationFrame(loop);
 
-  rock.rotation.y += 0.001;
-  // rock.rotation.y += 0.001;
+  // console.log(camera.position, camera.rotation)
+
+  // rockGroup.rotation.y += 0.001;
 };
 loop();
